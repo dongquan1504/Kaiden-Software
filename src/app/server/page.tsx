1b2c2e282b6c4c6cc5e51fa099a9fddfa750a379
Dương Đông Quân <72091817+dongquan1504@users.noreply.github.com>
2024-12-18 19:34:16 +0700

    Update page.tsx

--- src/app/server/page.tsx
+++ src/app/server/page.tsx
@@ -1,14 +1,33 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client"
-import { useEffect } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+// import MessageReceiver from '@/components/section/post-message-api';
 
 const IframePage = () => {
+  const iframeRef = useRef<HTMLIFrameElement>(null);
+  // const [isSendMessage, setIsSendMessage] = useState<boolean>(false);
+  const [message, setMessage] = useState<string>('');
+  const [receiveMessage, setReceiveMessage] = useState<string>('');
+
+  const sendMessage = () => {
+    if (iframeRef.current) {
+      // setIsSendMessage(true);
+      iframeRef.current.contentWindow?.postMessage(message, '*');
+    }
+  };
+
   useEffect(() => {
     const handleMessage = (event: MessageEvent) => {
-      console.log('Hello, I have receive your message, are you type: \"', event.data, "\" ?");
+      // Check if event.data is an object and convert it to a string
+      if (typeof event.data === 'string') {
+        setReceiveMessage(event.data);
+      }
     };
-
+  
     window.addEventListener('message', handleMessage);
-
+  
     return () => {
       window.removeEventListener('message', handleMessage);
     };
@@ -16,7 +35,15 @@ const IframePage = () => {
 
   return (
     <div>
-      <h1>Iframe Page</h1>
+      <h1>{receiveMessage}</h1>
+      <div className='flex justify-between'>
+      <Input value={message} onChange={(e: any) => setMessage(e.target.value)} />
+      <Button onClick={sendMessage}>Send Message</Button>
+      </div>
+
+      <iframe ref={iframeRef} src="http://localhost:9000/review" style={{ width: '100%', height: '500px', border: '1px solid black' }} />
+      {/* {isSendMessage &&
+        <iframe ref={iframeRef} src="http://localhost:9000/review" style={{ width: '100%', height: '500px', border: '1px solid black' }} />} */}
     </div>
   );
 };
